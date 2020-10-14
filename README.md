30bf013b65d0858f64c6e44801db75730cb07ddb
Patrick Wang <patrick.wang@isep.fr>
2020-10-14 15:07:11 +0200

    Orientation works for v2

--- README.md
+++ README.md
@@ -6,3 +6,37 @@ This application is just a toy application to showcase how we can dynamically ch
 interface by interacting with selection widgets (and in this case, `RadioButton`).
 
 It is also a nice way to introduce images and `ImageView`.
+
+
+## Version 2 (with orientation)
+
+In this second version of Pokemon Starters, we have worked with orientation to show a small description of
+the Pokémon that is currently selected.
+
+### How to know which Pokémon is selected?
+
+To answer this question, we have created a class variable `String selectedPokemon` which will simply take
+the name of the selected pokémon. It is this variable (and its value) that will be saved on the `savedInstanceState` bundle.
+
+This variable is set in different places:
+
+- Either when a `RadioButton` is clicked;
+- Or directly in the `onSaveInstanceState()` method, if we rotate the screen without having interacted with the `RadioButtons`.
+
+### How do we update the UI?
+
+I have implemented a new method called `updateDisplayAfterConfigurationChange()` which is called by the `onRestoreInstanceState()` method.
+The logic of this new method is as follows:
+
+1. Whether we are in portrait or landscape mode, we can set the image resource of our `imageView` with the contents of `selectedPokemon`;
+2. If we are in portrait mode:
+
+    1. We can benefit from the fact that the `ids` of our `RadioButtons` are all of the following format: `pokemonName_button`
+    to retrieve the `RadioButton` that corresponds to the `selectedPokemon`.
+    2. We can then dynamically set its `isChecked` attribute to `true`.
+    
+3. If we are in landscape mode:
+
+    1. We can also benefit from the fact that the `ids` of our descriptions are all of the following format: `pokemonName_description`
+    to retrieve the description that corresponds to the `selectedPokemon`.
+    2. We can then dynamically set the text of the `descriptionTextView` to the description retrieved.
--- app/src/main/java/fr/isep/ii3510/pokemonstarters/MainActivity.java
+++ app/src/main/java/fr/isep/ii3510/pokemonstarters/MainActivity.java
@@ -1,13 +1,21 @@
 package fr.isep.ii3510.pokemonstarters;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String BULBASAUR = "bulbasaur";
+    public static final String CHARMANDER = "charmander";
+    public static final String SQUIRTLE = "squirtle";
     private ImageView imageView;
+    private String selectedPokemon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +24,56 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // If we change the orientation without having interacted with the RadioButtons
+        if (selectedPokemon == null) {
+            selectedPokemon = BULBASAUR;
+        }
+        outState.putString("Pokemon", selectedPokemon);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        selectedPokemon = savedInstanceState.getString("Pokemon");
+        updateDisplayAfterConfigurationChange();
+    }
+
+    private void updateDisplayAfterConfigurationChange() {
+        // The image needs to be updated no matter which screen orientation we have, because both UIs have an ImageView
+        imageView.setImageResource(getResources().getIdentifier(selectedPokemon, "drawable", getPackageName()));
+
+        // If screen orientation is in portrait, we have to check the appropriate RadioButton
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            String radioButtonIdName = selectedPokemon + "_button";
+            RadioButton checkedButton = findViewById(getResources().getIdentifier(radioButtonIdName, "id", getPackageName()));
+            checkedButton.setChecked(true);
+        } else {
+            // Else, we have to display the appropriate Pokémon description
+            String descriptionId = selectedPokemon + "_description";
+            TextView textView = findViewById(R.id.description_textView);
+            textView.setText(getString(getResources().getIdentifier(descriptionId, "string", getPackageName())));
+        }
+    }
+
+
     // Each of the following method is a listener attached to one of the RadioButton
     // declared on the user interface
     // In this case, we simple change the image resource to display the corresponding image
     public void bulbasaurClicked(View view) {
         imageView.setImageResource(R.drawable.bulbasaur);
+        selectedPokemon = BULBASAUR;
     }
 
     public void charmanderClicked(View view) {
         imageView.setImageResource(R.drawable.charmander);
+        selectedPokemon = CHARMANDER;
     }
 
     public void squirtleClicked(View view) {
         imageView.setImageResource(R.drawable.squirtle);
+        selectedPokemon = SQUIRTLE;
     }
 }
